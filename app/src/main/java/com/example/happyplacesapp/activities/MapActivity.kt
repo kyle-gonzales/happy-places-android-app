e08f6298472190022723417f4e53c44c64c92d25
Kyle Gonzales <kyleangelog@gmail.com>
2023-02-16 16:53:21 +0800

    added marker and zoom animatioon map for happy place location

--- app/src/main/java/com/example/happyplacesapp/activities/MapActivity.kt
+++ app/src/main/java/com/example/happyplacesapp/activities/MapActivity.kt
@@ -1,13 +1,21 @@
 package com.example.happyplacesapp.activities
 
+import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.happyplacesapp.R
 import com.example.happyplacesapp.databinding.ActivityMapBinding
 import com.example.happyplacesapp.happy_place_database.HappyPlaceEntity
 import com.example.happyplacesapp.utils.Constants
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private var binding : ActivityMapBinding? = null
     private var happyPlaceItem : HappyPlaceEntity? = null
@@ -28,10 +36,25 @@ class MapActivity : AppCompatActivity() {
         } else {
             supportActionBar?.title = "Your Happy Place"
         }
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         binding = null
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        val location = LatLng(happyPlaceItem!!.latitude, happyPlaceItem!!.longitude)
+        map?.addMarker(MarkerOptions().position(location).title(happyPlaceItem!!.name))
+
+        val newLatLngZoom = CameraUpdateFactory.newLatLngZoom(location, 15.0f)
+
+//        map?.moveCamera(CameraUpdateFactory.newLatLng(location))
+//        map?.moveCamera(newLatLngZoom)
+
+        map?.animateCamera(newLatLngZoom)
     }
 }
